29addb6d0f3f0e0cbbed5ba2266bb17455cf15f3
Churjan <churjan@163.com>
2024-06-26 17:10:54 +0800

    feat:添加国际化

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,28 +2,52 @@ import {
   ApplicationConfig,
   provideZoneChangeDetection,
   importProvidersFrom,
+  inject,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideNzIcons } from './icons-provider';
-import { zh_CN, provideNzI18n } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideNzIcons } from './icons-provider';
 
+/** 配置 ng-zorro-antd 国际化 **/
+import { en_US, NZ_I18N, zh_CN } from 'ng-zorro-antd/i18n';
+
+/** 导入需要使用的 Angular 语言包 **/
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(en);
 registerLocaleData(zh);
 
+const i18nProvider = {
+  provide: NZ_I18N,
+  useFactory: () => {
+    const localId = localStorage.getItem('language') || navigator.language;
+    console.log('localId', localId);
+    switch (localId) {
+      case 'en':
+        return en_US;
+      case 'zh':
+        return zh_CN;
+      default:
+        return zh_CN;
+    }
+  },
+};
+
+const providers: any[] = [
+  provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  provideNzIcons(),
+  importProvidersFrom(FormsModule),
+  provideAnimationsAsync(),
+  provideHttpClient(),
+];
+
+providers.push(i18nProvider);
+
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideNzIcons(),
-    provideNzI18n(zh_CN),
-    importProvidersFrom(FormsModule),
-    provideAnimationsAsync(),
-    provideHttpClient(),
-  ],
+  providers,
 };
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { ModulesModule } from './modules/modules.module';
 
 @NgModule({
-  exports: [ModulesModule],
+  exports: [FormsModule, ModulesModule],
 })
 export class SharedModule {}
